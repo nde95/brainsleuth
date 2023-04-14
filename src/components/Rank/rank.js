f745ad205769749085b5365a6c7144d1517eea8a
nde95 <ndennison268@gmail.com>
2023-04-14 14:03:50 -0400

    UI more dynamic to user

--- src/components/Rank/rank.js
+++ src/components/Rank/rank.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({ name, entries }) => {
     return(
         <div>
             <div className='white f3'>
-                {'Nigel, you are currently ranked...'}
+                {`${name}, your entry count is currently...`}
             </div>
             <div className='white f1'>
-                {'#1'}
+                {`${entries}`}
             </div>
         </div>
     );
