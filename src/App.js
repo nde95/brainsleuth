16676be49f3abfe9dfb25dc21896f9cb1dace2a8
nde95 <ndennison268@gmail.com>
2023-04-21 16:01:49 -0400

    added catch

--- src/App.js
+++ src/App.js
@@ -67,6 +67,7 @@ class App extends Component {
         .then(count => {
           this.setState(Object.assign(this.state.user, { entries: count}));
         })
+        .catch(console.log)
       })
       .catch(error => console.log(error));
   }
